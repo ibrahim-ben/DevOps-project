39b8cc40a475520aa89cb650725b7055e9a77d1b
ibrahim-ben <66121733+ibrahim-ben@users.noreply.github.com>
2023-10-31 22:35:41 +0100

    newchanges

--- DevOps_Project_Front/src/app/services/stock.service.ts
+++ DevOps_Project_Front/src/app/services/stock.service.ts
@@ -6,7 +6,7 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class StockService {
-  private apiUrl = 'http://backend:8080/stock';
+  private apiUrl = 'http://backend:8081/stock';
 
   constructor(private http: HttpClient) {
   }
