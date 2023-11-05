de506d3db9cdb69e488492122d7949bfebce0db1
ibrahim-ben <66121733+ibrahim-ben@users.noreply.github.com>
2023-11-05 15:43:40 +0100

    05-11-integratingonPOM

--- DevOps_Project/src/main/java/tn/esprit/devops_project/controllers/StockController.java
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/controllers/StockController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import tn.esprit.devops_project.entities.Stock;
 import tn.esprit.devops_project.services.Iservices.IStockService;
 import java.util.List;
+import io.prometheus.client.Counter;
+import io.prometheus.client.spring.web.PrometheusTimeMethod;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -13,6 +15,18 @@ import java.util.List;
 public class StockController {
 
     IStockService stockService;
+    private final Counter requestsTotal = Counter.build()
+        .name("myapp_requests_total")
+        .help("Total HTTP requests.")
+        .labelNames("method")
+        .register();
+
+    @PrometheusTimeMethod(name = "myapp_request_duration_seconds", help = "Request duration in seconds.")
+    @GetMapping("/myendpoint")
+    public String myEndpoint() {
+        requestsTotal.labels("GET").inc();
+        return "Response";
+    }
 
     @PostMapping("/stock")
     public Stock addStock(@RequestBody Stock stock){
@@ -28,6 +42,8 @@ public class StockController {
     public List<Stock> retrieveAllStock(){
         return stockService.retrieveAllStock();
     }
+
+    
 
 
 }
